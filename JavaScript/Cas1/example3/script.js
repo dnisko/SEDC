13ee8580a9d806a2717b98cea255d887c4c8ff45
dnisko <dnisko@hotmail.com>
2023-12-04 20:49:50 +0100

    JavaScript Cas 1

--- JavaScript/Cas1/example3/script.js
+++ JavaScript/Cas1/example3/script.js
@@ -8,3 +8,13 @@ console.log("Feet =", feet, "\nto meters =", feetToMeters);
 let meters = 80;
 let metersToFeet = meters / convertToMeters;
 console.log("Meters =", meters, "\nto feet =", metersToFeet);
+
+
+console.log("----------------------------------------")
+
+let sideA = 5;
+let sideB = 10;
+
+let area = sideA * sideB;
+
+console.log("The area of A: ", sideA, " and B: ", sideB, " is: ", area);
